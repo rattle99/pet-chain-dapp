29ced068c4475458caa6d5edd17c85d6acbbadb3
Diego Hernandes <iamhernandev@gmail.com>
2019-01-05 00:27:54 -0200

    update web3 plugin

--- src/web3.js
+++ src/web3.js
@@ -5,10 +5,18 @@ import Web3 from 'web3'
 
 // init web3 function.
 const initWeb3 = (Vue) => {
-  // get web3 provider.
-  const provider = Web3.givenProvider
-  // start web3 instance with the provider.
-  Vue.prototype.web3 = new Web3(provider)
+  // start web3 instance with the given provider.
+  const web3 = new Web3(Web3.givenProvider)
+
+  // assign web3 on vue prototype.
+  Vue.prototype.web3 = web3
+  // assign provider on vue prototype.
+  Vue.prototype.currentProvider = web3.currentProvider
+
+  // let web3 on window when not production mode (easier deb debug).
+  if (process.env.NODE_ENV !== 'production') {
+    window.web3 = web3
+  }
 }
 
 // enable web3 plugin.
